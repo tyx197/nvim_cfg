7378b4ba4e944f3ff89e3b232b7165c1a5882c2e
tyx197 <ytaoai@connect.ust.hk>
2023-03-31 01:18:05 +0800

    Change telescope keymap, <leader>fh mapped to oldfiles, <leader>ft mapped to help_tags.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -8,4 +8,5 @@ vim.keymap.set('n', '<Space><Space>', ':NvimTreeToggle<CR>') -- previous buffer
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ft', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fh', builtin.oldfiles, {})
